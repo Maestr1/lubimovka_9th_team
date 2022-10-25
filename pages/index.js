870c334ddb2057e990fbc3f754dcaa85e82b9c25
Eugene Avdeev <ea.avdeev@gmail.com>
2022-10-25 14:35:13 +0300

    nav menu Pperfect

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 const burgerBtn = document.querySelector('.navbar__burger')
 const mobileMenuOverlay = document.querySelector('.navbar__overlay-mobile')
+// x
 
+// overlay mobile menu open / close  - - -
 function handleBurgerBtn() {
-  openMobileMenu()
+  toggleMobileMenu()
+  toggleBurgerButton()
 }
 
-function openMobileMenu() {
+function toggleMobileMenu() {
   mobileMenuOverlay.classList.toggle('navbar__overlay-mobile_opened')
+}
+function toggleBurgerButton() {
+  burgerBtn.classList.toggle('navbar__burger_close')
 }
 
 burgerBtn.addEventListener('click', handleBurgerBtn)
+
+// burger button status
+
+
+// navbar__burger_close
