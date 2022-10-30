fcd523301fc5ad2bfddf76d73f3a85b049b1b688
Eugene Avdeev <ea.avdeev@gmail.com>
2022-10-30 14:36:50 +0300

    fix: burger btn statuses

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const burgerBtn = document.querySelector('.burger')
 const mobileMenuOverlay = document.querySelector('.burger__nav')
 
-// overlay mobile menu open / close  - - -
+// overlay mobile menu open / close
 function handleBurgerBtn() {
   toggleMobileMenu()
   toggleBurgerButton()
@@ -11,12 +11,9 @@ function toggleMobileMenu() {
   mobileMenuOverlay.classList.toggle('burger__nav_opened')
 }
 
+// burger button status Opened/Closed
 function toggleBurgerButton() {
-  burgerBtn.classList.toggle('burger_open')
+  burgerBtn.classList.toggle('burger_close')
 }
 
 burgerBtn.addEventListener('click', handleBurgerBtn)
-
-// burger button status
-
-// navbar__burger_close
